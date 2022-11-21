93fac623a88e84ee8dea0be5819fe3a125121b69
Nico Krause <inspiraluna@users.noreply.github.com>
2022-11-21 22:28:24 +0800

    fixed missing ssl-dhparams.pem and options-ssl-nginx.conf

--- nginx/docker-entrypoint.d/001-parse-template.sh
+++ nginx/docker-entrypoint.d/001-parse-template.sh
@@ -3,8 +3,8 @@ echo 'entry point was ${SERVER_NAME} running' > /tmp/was-running-${SERVER_NAME}.
 envsubst '${SERVER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/${SERVER_NAME}.conf
 data_path=/etc/letsencrypt
 mkdir -p "$data_path/conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+curl -s curl -s https://raw.githubusercontent.com/certbot/certbot/0.29.x/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/1.32.x/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 echo 'running nginx now and reload every 6 hours'
 while :; do sleep 6h & wait ${!}; nginx -s reload; done & nginx -g "daemon off;"
 
