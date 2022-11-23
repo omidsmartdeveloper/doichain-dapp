2941426260a72a61c773ae7818735ffecd4febc3
Nico Krause <inspiraluna@users.noreply.github.com>
2022-11-23 15:52:31 +0800

    changed port and prevented certificate generation at every start of nginx

--- nginx/docker-entrypoint.d/001-create-temp-certs.sh
+++ nginx/docker-entrypoint.d/001-create-temp-certs.sh
@@ -5,11 +5,13 @@ rsa_key_size=4096
 email=$DAPP_SMTP_DEFAULT_FROM # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p $path
-openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+if [ ! -d "$path" ]; then
+  echo "### Creating dummy certificate for $domains ..."
+  mkdir -p $path
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout "$path/privkey.pem" \
     -out "$path/fullchain.pem" \
     -subj '/CN=localhost'
-echo "done"
+  echo "done"
+fi
